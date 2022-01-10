e61b7b1e7eba386102a2acddc4b682577b3a69ee
Yogi Hariyani <hariyaniyogi7292@gmail.com>
2022-01-10 17:07:36 +0530

    starting Backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
-
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Router>
+    <Header />
+    <main className="py-4"> 
+    <Container> 
+    <Routes>
+    <Route path='/' element={<HomeScreen />} exact />
+    <Route path='/home' element={<HomeScreen />} />
+    <Route path='/Product/:id' element={<ProductScreen />} />
+    
+    </Routes>
+    </Container>
+    </main>
+    
+    <Footer />
+    </Router>
   );
 }
 
