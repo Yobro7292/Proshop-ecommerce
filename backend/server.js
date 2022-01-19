b9795a6fafe996c8f3090928a6018e1a919a6ea3
Yogi Hariyani <hariyaniyogi7292@gmail.com>
2022-01-19 18:08:48 +0530

    connecting DB

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import products from './data/products.js'
 
+dotenv.config()
+connectDB()
 const app = express()
 
 app.get('/', (req, res) =>{
     res.send('Api Running...')
 })
 app.get('/api/products', (req, res) =>{
-    res.json(products)
+    res.json(products) 
 })
 app.get('/api/products/:id', (req, res) =>{
     const product = products.find(p => p._id === req.params.id)
     res.json(product)
 })
 
-app.listen(5000, console.log('Server Running in 5000'))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} ${PORT}`))
