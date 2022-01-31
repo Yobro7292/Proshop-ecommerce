da48ff61d37b823e70acc8934ef8db1323c1f037
Yogi Hariyani <hariyaniyogi7292@gmail.com>
2022-01-31 12:16:09 +0530

    test rename
    just testing

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -19,11 +19,11 @@ const HomeScreen = () => {
     
     return (
         <>
-            <h1>Latest Products</h1>
+            <h1>Latest Products Available</h1>
             {loading ? <Loader/> : error ? <Message varient='danger'></Message> : <Row>
                 {products.map((product) => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
+                        <Product product={product} /> 
                     </Col>
                 ))}
             </Row>}
